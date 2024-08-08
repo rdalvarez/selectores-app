416e49fe13f11b68424b831a63da1730a8ed80c5
David <alv.ricardodavid@gmail.com>
2024-08-07 21:17:32 -0300

    Selector de regiones

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Region } from '../interfaces/country.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountriesService {
+  private _regions: Region[] = [
+    Region.Africa,
+    Region.Americas,
+    Region.Asia,
+    Region.Europe,
+    Region.Oceania,
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  public get regions(): Region[] {
+    return [...this._regions];
+  }
 }
